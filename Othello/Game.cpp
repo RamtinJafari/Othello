18b7f65428ebb5b5a81915b4a3555e5244dce950
Ramtin Jafari <ramtinjafari86@gmail.com>
2026-01-01 16:40:29 +0330

    created Game.cpp (menu) to handle exit and setting options

---
+++ Othello/Game.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include "/codes/Structures/Config.h"
+#include "/codes/ConfigModifier.h"
+#include <conio.h>
+
+int main()
+{
+    clrscr();
+    
+    Config::initialize();
+
+    int chosenOption = 0;
+
+    while (true)
+    {
+        clrscr();
+
+        cout << "Welcome to Othello game!" << endl
+            << (chosenOption == 0 ? "■":"□") << " new game" << endl
+            << (chosenOption == 1 ? "■":"□") << " load game" << endl
+            << (chosenOption == 2 ? "■":"□") << " help" << endl
+            << (chosenOption == 3 ? "■":"□") << " games history" << endl
+            << (chosenOption == 4 ? "■":"□") << " settings" << endl
+            << (chosenOption == 5 ? "■":"□") << " exit" << endl;
+
+        int userInput = getch();
+
+        if (userInput == static_cast<int>('w') || userInput == static_cast<int>('↑'))
+        {
+            if (chosenOption > 0)
+            {
+                chosenOption--;
+                continue;
+            }
+        }
+
+        if (userInput == static_cast<int>('s') || userInput == static_cast<int>('↓'))
+        {
+            if (chosenOption < 5)
+            {
+                chosenOption++;
+                continue;
+            }
+        }
+
+         if (userInput == static_cast<int>('⏎'))
+        {
+            if (chosenOption == 0)
+            {
+                // new game
+            }
+            else if (chosenOption == 1)
+            {
+                // load game
+            }
+            else if (chosenOption == 2)
+            {
+                // help
+            }
+            else if (chosenOption == 3)
+            {
+                // games history
+            }
+            else if (chosenOption == 4)
+            {
+                ConfigModifier::execute();
+            }
+            else if (chosenOption == 5)
+            {
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
