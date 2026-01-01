1b6fbc7f0cbc6929564090b449e8941ec0bca460
Ramtin Jafari <ramtinjafari86@gmail.com>
2026-01-01 19:39:31 +0330

    added Print to Utilities for outputing a string several times

---
+++ Othello/codes/Utilities/Print.cpp
@@ -0,0 +1,6 @@
+#include <string>
+
+void print(std::string subject, int count)
+{
+    for (int i = 0; i < count; i++) cout << subject;
+}
---
+++ Othello/codes/Utilities/Print.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void print(std::string subject, int count);
