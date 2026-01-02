723d009b074e240ab2d02dc9e9dec9fd5b23deb5
Ramtin_Jafari <ramtinjafari86@gmail.com>
2026-01-02 21:24:19 +0330

    created bot logic for easy and medium difficulty

--- Othello/codes/Structures/Bot.cpp
+++ Othello/codes/Structures/Bot.cpp
@@ -3,6 +3,7 @@
 #include "../Utilities/IntToStr.h"
 #include "../Utilities/StrToInt.h"
 #include <string>
+#include <random>
 
 struct Bot
 {
@@ -10,53 +11,103 @@ struct Bot
     int difficulty;
 
     public:
-    void decide(Board board)
+    void decide(Board board, char color)
     // the fucntion called by GameMaster when it's bot's turn to move
     // this function should be called when prepareBoardForMove is called
     // so bot will work as expected
     {
         if (difficulty == 1) // easy
         {
-            easyDecide(board);
+            easyDecide(board, color);
         }
         else if (difficulty == 2) // medium
         {
-            mediumDecide(board);
+            mediumDecide(board, color);
         }
         else if (difficulty == 3) // hard
         {
-            hardDecide(board);
+            hardDecide(board, color);
         }
     }
 
+
     private:
-    void easyDecide(Board borad)
+    void easyDecide(Board board, char color)
     // the decision a bot would make in easy mode
     // the act is based on a random choice
     {
+        int validMovesCount = board.countValidMoves();
 
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<int> distrib(0, validMovesCount-1);        
+        int randomNumber = distrib(gen);
+
+        int moveNumber = 0;
+        for (int y = 0; y < board.BoardSize; y++)
+        {
+            for (int x = 0; x < board.BoardSize; x++)
+            {
+                if (board.validMovesGrid[y][x] == '⦻')
+                {
+                    if (moveNumber == randomNumber)
+                    {
+                        board.putPiece(x, y, color);
+                    }
+                    else
+                    {
+                        moveNumber++;
+                    }
+                }
+            }
+        }
     }
 
+
     private:
-    void mediumDecide(Board board)
+    void mediumDecide(Board board, char color)
     // the decision a bot would make in medium mode
     // the act is based on how much gain each move will have
+    // the move with the most gain is made
     {
+        int moveX, moveY;
+        int maxGain = 0;
 
+        for (int y = 0; y < board.BoardSize; y++)
+        {
+            for (int x = 0; x < board.BoardSize; x++)
+            {
+                if (board.validMovesGrid[y][x] == '⦻')
+                {
+                    int gain = board.isValid(x, y, color);
+                    if (gain > maxGain)
+                    {
+                        moveY = y;
+                        moveX = x;
+                        maxGain = gain;
+                    }
+                }
+            }
+        }
+
+        board.putPiece(moveX, moveY, color);
     }
+
 
     private:
-    void hardDecide(Board board)
+    void hardDecide(Board board, char color)
     // the decision a bot would make in hard mode
     {
-
+        // do later
     }
+
 
     public:
     std::string retrieveBot()
     {
         return name + "\"" + intToStr(difficulty);
     } 
+
 
     public:
     void loadBot(std::string bot)
